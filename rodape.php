4cd3d06fc44efe3921a8e95e57d9fc0a24e85a34
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-03 20:43:32 -0300

    rodape.php

---
+++ rodape.php
@@ -0,0 +1,23 @@
+<footer>
+        <div class="footer-container">
+            <div>
+                <h3 class="integrantes">Integrantes</h3>
+                <ul class="lista">
+                    <li class="nome">Alessandra Cristina da Silva Pereira</li>
+                    <li class="nome">Aluno de Análise e Desenvolvimento de Sistemas de 2024.2</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+<!--Página OK-->
