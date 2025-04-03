63e5a7300c4f55cd4bfe3d697d1211c20f62d306
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-03 20:40:29 -0300

    Update and rename exercicio2.html to exercicio2.php

---
+++ exercicio2.php
@@ -0,0 +1,22 @@
+<?php include 'cabecalho.php'?>
+
+    <form action="">
+        <fieldset id="caixa">
+            <h4>Formulário de Autenticação:</h4>
+            <strong><p>Login:</p></strong>
+            <input type="text" size="50">
+            <br>
+            <br>
+            <strong><p>Senha:</p></strong>
+            <input type="password" size="50">
+            <br>
+            <br>
+            <br>
+            <button type="submit" class="botao1">ENVIAR</button>
+            <button type="button" class="botao2">VOLTAR</button>
+        </fieldset>
+    </form>
+</body>
+</html>
+
+<?php include 'rodape.php' ?>
