1266ddf7c9077bb03ceb58667e5866a87b7ae23e
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-03 20:41:49 -0300

    exercicio3.php

---
+++ exercicio3.php
@@ -0,0 +1,20 @@
+<?php include 'cabecalho.php'?>
+
+    <form action="">
+        <fieldset id="caixa">
+            <h4>Valor da Densidade:</h4>
+            <strong><p>Massa:</p></strong>
+            <input type="number" size="50">
+            <br>
+            <br>
+            <strong><p>Volume:</p></strong>
+            <input type="number" size="50">
+            <br>
+            <br>
+            <br>
+            <button type="submit" class="botao1">ENVIAR</button>
+            <button type="button" class="botao2">VOLTAR</button>
+        </fieldset>
+    </form>
+
+<?php include 'rodape.php' ?>
