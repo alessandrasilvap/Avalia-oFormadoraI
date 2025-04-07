d6a1637c940e85f537e30332dce0539acfac7c38
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-07 19:56:57 -0300

    ava1.py

---
+++ Avaliacao1 - FrontEnd /ava.py
@@ -0,0 +1,18 @@
+#Exercício 1 - exemplo fácil:
+x = 46843
+y = 74864
+
+if x > y:
+    print("X é maior.")
+else:
+    print("Y é maior.")
+
+#Exercício 1 - exemplo com IF, ELIF e ELSE:
+numero = int(input("Digite um número: "))
+
+if numero >= 7:
+  print("Aprovado!")
+elif numero <= 4:
+  print("Reprovado.")
+else:
+  print("Fará a prova novamente!")
