1a7c3efcfa602704535714d16b11f9feb2916182
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-07 19:57:19 -0300

    ava2.php

---
+++ Avaliacao1 - FrontEnd /ava2.php
@@ -0,0 +1,27 @@
+#Exercício 2 - Usando WHILE:
+contador = 0
+
+while contador < 10:
+    print(f"Valor do contador é {contador}")
+    contador += 1
+
+
+
+x = 5
+while x > 0:
+    x -= 1
+    print(x)
+
+
+#Exercício 2 - Usando FOR:
+nome = "Alessandra"
+
+for caractere in nome:
+    print(caractere)
+
+
+
+for x in range(50):
+    print(x)
+else:
+    print("Fim do loop!")
